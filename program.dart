e2a9dc0ba649c6a50e4d0d9266f7183374e9abac
Sanfotsi <rradityaathariz@gmail.com>
2026-06-25 11:20:48 +0700

    2. 6/25/2026

--- program.dart
+++ program.dart
@@ -10,27 +10,19 @@ class Persona{
 
     Persona(this.name, this.hp, this.sp, this.skills);
 
-    void magic(Persona target, String skill){
-        int power = magical[skill]!['Power'] as int;
-        int accuracy = magical[skill]!['Accuracy'] as int;
-        int cost = magical[skill]!['Cost'] as int;
+    void skill(Persona target, String skill){
+        int power = effect[skill]!['Power'] as int;
+        int accuracy = effect[skill]!['Accuracy'] as int;
+        int cost = effect[skill]!['Cost'] as int;
+        String type = effect[skill]!['Type'] as String;
 
         if (Random().nextInt(100) + 1 <= accuracy){
-            sp -= cost;
-            target.hp = max(0, target.hp - power);
-            print('${this.name} casted ${skill} on ${target.name}');
-        } else {
-            print('${this.name} casted ${skill} on ${target.name}, but it missed!');
-        }
-    }
-
-    void physique(Persona target, String skill){
-        int power = physical[skill]!['Power'] as int;
-        int accuracy = physical[skill]!['Accuracy'] as int;
-        int cost = physical[skill]!['Cost'] as int;
-
-        if (Random().nextInt(100) + 1 <= accuracy){
-            hp -= hp * (cost ~/ 100);
+            switch (type){
+                case 'Magic':
+                    sp -= cost;
+                case 'Physical':
+                    hp -= hp * (cost ~/ 100);
+            }
             target.hp = max(0, target.hp - power);
             print('${this.name} casted ${skill} on ${target.name}');
         } else {
@@ -53,62 +45,33 @@ class Persona{
 }
 
 void main(){
-    Persona Yu = Persona('Izanagi', 100, 100, ['Zio', 'Cleave']);
-    Persona Yosuke = Persona('Jiraiya', 100, 100, ['Garu', 'Bash', 'Dia']);
-    Persona Chie = Persona('Tomoe', 100, 100, ['Skewer']);
-    Persona Yukiko = Persona('Konohana Sakuya', 100, 100, ['Dia', 'Agi']);
-
     Map<String, Persona> characters = {
-        'Yu': Yu,
-        'Yosuke': Yosuke,
-        'Chie': Chie,
-        'Yukiko': Yukiko 
+        'Yu': Persona('Izanagi', 100, 100, ['Zio', 'Cleave']),
+        'Yosuke': Persona('Jiraiya', 100, 100, ['Garu', 'Bash', 'Dia']),
+        'Chie': Persona('Tomoe', 100, 100, ['Skewer']),
+        'Yukiko': Persona('Konohana Sakuya', 100, 100, ['Dia', 'Agi']) 
     };
-
-    Persona opponent1 = Persona('Magatsu-Izanagi', 100, 1000, ['Zio', 'Cleave', 'Dia']);
-    Persona opponent2 = Persona('Magatsu-Izanagi', 100, 1000, ['Garu', 'Bash', 'Dia']);
-    Persona opponent3 = Persona('Magatsu-Izanagi', 100, 1000, ['Bufu', 'Skewer', 'Dia']);
 
     Map<String, Persona> opponents = {
-        'opponent1': opponent1,
-        'opponent2': opponent2,
-        'opponent3': opponent3
+        '1': Persona('Magatsu-Izanagi', 100, 100, ['Garu', 'Bash']),
+        '2': Persona('Magatsu-Izanagi', 100, 100, ['Bufu', 'Skewer']),
+        '3': Persona('Magatsu-Izanagi', 100, 100, ['Agi', 'Cleave']),
     };
 
-    while (Yu.hp > 0 && opponents.values.any((opponent) => opponent.hp > 0)){
+    while (characters['Yu']!.hp > 0 && opponents.values.any((opponent) => opponent.hp > 0)){
+        for (var i in characters.values){
+          print(i.info());
+        }
+
+        for (var i in opponents.values){
+          print(i.info());
+        }
+        
         for (var i in characters.values){
             stdout.write('${i.name}: ');
             String move = stdin.readLineSync()!;
             switch (move){
-                case 'Magic':
-                    stdout.write('Skill: ');
-                    String choice = stdin.readLineSync()!;
-                    while (!i.skills.contains(choice)){
-                        stdout.write('Skill: ');
-                        choice = stdin.readLineSync()!;
-                    };
-                    stdout.write('Target: ');
-                    String target = stdin.readLineSync()!;
-                    while (!opponents.keys.contains(target)){
-                        stdout.write('Target: ');
-                        target = stdin.readLineSync()!;
-                    };
-                    i.magic(opponents[target]!, choice);
-                case 'Physique':
-                    stdout.write('Skill: ');
-                    String choice = stdin.readLineSync()!;
-                    while (!i.skills.contains(choice)){
-                        stdout.write('Skill: ');
-                        choice = stdin.readLineSync()!;
-                    };
-                    stdout.write('Target: ');
-                    String target = stdin.readLineSync()!;
-                    while (!opponents.keys.contains(target)){
-                        stdout.write('Target: ');
-                        target = stdin.readLineSync()!;
-                    };
-                    i.physique(opponents[target]!, choice);
-                case 'Heal':
+                case 'Dia':
                     stdout.write('Target: ');
                     String target = stdin.readLineSync()!;
                     while (!characters.keys.contains(target)){
@@ -116,25 +79,27 @@ void main(){
                         target = stdin.readLineSync()!;
                     };
                     i.heal(characters[target]!, 'Dia');
+                default:
+                    while (!i.skills.contains(move)){
+                        stdout.write('Skill: ');
+                        move = stdin.readLineSync()!;
+                    };
+                    stdout.write('Target: ');
+                    String target = stdin.readLineSync()!;
+                    while (!opponents.keys.contains(target)){
+                        stdout.write('Target: ');
+                        target = stdin.readLineSync()!;
+                    };
+                    i.skill(opponents[target]!, move);
             }
         }
         for (var i in opponents.values){
-            switch (Random().nextInt(3)){
+            switch (Random().nextInt(2)){
                 case 0:
-                    i.magic(characters[characters.keys.toList()[Random().nextInt(characters.keys.length)]]!, 'Garu');
+                    i.skill(characters[characters.keys.toList()[Random().nextInt(characters.keys.length)]]!, 'Garu');
                 case 1:
-                    i.physique(characters[characters.keys.toList()[Random().nextInt(characters.keys.length)]]!, 'Bash');
-                case 2:
                     i.heal(opponents[opponents.keys.toList()[Random().nextInt(opponents.keys.length)]]!, 'Dia');
             }
-        }
-
-        for (var i in characters.values){
-          print(i.info());
-        }
-
-        for (var i in opponents.values){
-          print(i.info());
         }
     }
 }
--- skills.dart
+++ skills.dart
@@ -1,55 +1,56 @@
 const light = {
-    "Power": 40,
-    "Accuracy": 98,
-    "Cost": 4,
+    'Power': 40,
+    'Accuracy': 98,
+    'Cost': 4,
+    'Type': 'Magic'
 };
 
 const medium = {
-    "Power": 100,
-    "Accuracy": 98,
-    "Cost": 8
+    'Power': 100,
+    'Accuracy': 98,
+    'Cost': 8
 };
 
 const heavy = {
-    "Power": 160,
-    "Accuracy": 98,
-    "Cost": 12
+    'Power': 160,
+    'Accuracy': 98,
+    'Cost': 12
 };
 
 const severe = {
-    "Power": 200,
-    "Accuracy": 98,
-    "Cost": 48
+    'Power': 200,
+    'Accuracy': 98,
+    'Cost': 48
 };
 
-const magical = {
-  "Agi": light,
-  "Bufu": light,
-  "Zio": light,
-  "Garu": light
-};
-
-const physical = {
-    "Bash": {
-    "Power": 60,
-    "Accuracy": 100,
-    "Cost": 6,
+const effect = {
+  'Agi': light,
+  'Bufu': light,
+  'Zio': light,
+  'Garu': light,
+  'Bash': {
+    'Power': 60,
+    'Accuracy': 100,
+    'Cost': 6,
+    'Type': 'Physical'
   },
-  "Cleave": {
-    "Power": 65,
-    "Accuracy": 100,
-    "Cost": 5,
+  'Cleave': {
+    'Power': 65,
+    'Accuracy': 100,
+    'Cost': 5,
+    'Type': 'Physical'
   },
-  "Skewer": {
-    "Power": 70,
-    "Accuracy": 85,
-    "Cost": 5,
+  'Skewer': {
+    'Power': 70,
+    'Accuracy': 85,
+    'Cost': 5,
+    'Type': 'Physical'
   }
 };
 
 const healing = {
-  "Dia": {
-    "Power": 40,
-    "Cost": 3
+  'Dia': {
+    'Power': 40,
+    'Cost': 3
   }
 };
